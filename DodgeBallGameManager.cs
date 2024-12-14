183a1ee3f47b2b9677aafb507881ae7706f466ab
Brandon Withington <brandon.f.withington@gmail.com>
2024-12-13 16:40:29 -0800

    New dodgeball game manager and dodgeball functionality

---
+++ DodgeBallGameManager.cs
@@ -0,0 +1,311 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics;
+using ML.SDK;
+using TMPro;
+
+public class DodgeBallGameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject BlueTeleportObjectLocation;
+
+    [SerializeField]
+    private GameObject RedTeleportObjectLocation;
+
+
+    [SerializeField]
+    private GameObject RespawnPosition;
+
+    [SerializeField]
+    private ML.SDK.MLClickable taggersClickable;
+
+    [SerializeField]
+    private ML.SDK.MLClickable runnersClickable;
+
+    [SerializeField]
+    private ML.SDK.MLClickable StartGameClickable;
+
+    [SerializeField]
+    private TextMeshPro timeText;
+
+    public List<MLPlayer> playersArray = new List<MLPlayer>();
+
+    public float TimeLimit;
+    public TextMeshPro GameStatusText;
+    public TextMeshPro BlueTeamString;
+    public TextMeshPro RedTeamString;
+
+    private Stopwatch stopwatch;
+    private List<string> blueTeam = new List<string>();
+    private List<string> redTeam = new List<string>();
+
+    const string EVENT_SELECT_TEAM = "TeamSelectEvent";
+    private EventToken tokenSelectTeam;
+    const string EVENT_START_GAME = "EventStartGame";
+    private EventToken tokenStartGame;
+
+    const string EVENT_TELEPORT_PLAYERS_INTO_ARENA = "TeleportPlayersEvent";
+    private EventToken tokenTeleportPlayers;
+
+    const string EVENT_TELEPORT_PLAYERS_BACK_TO_SPAWN = "TeleportPlayersEvent_BackToSpawn";
+    private EventToken tokenTeleportPlayers_backToSpawn;
+
+    public bool isGameActive = false;
+
+    public void OnStartGameEvent(object[] args)
+    {
+        isGameActive = true;
+        stopwatch.Start();
+        TeleportPlayersToArena();
+    }
+
+    public void OnplayerClickStart(MLPlayer player)
+    {
+        // Player started the game
+        this.InvokeNetwork(EVENT_START_GAME, EventTarget.All, null);
+        this.InvokeNetwork(EVENT_TELEPORT_PLAYERS_INTO_ARENA, EventTarget.All, null);
+    }
+
+    public void OnTeleportPlayers(object[] args)
+    {
+        TeleportPlayersToArena();
+    }
+
+    public void OnTeleportPlayersBackToSpawn(object[] args)
+    {
+        TeleportPlayersToSpawn();
+    }
+
+    void Start()
+    {
+        stopwatch = new Stopwatch();
+
+        if (taggersClickable != null)
+        {
+            taggersClickable.OnPlayerClick.AddListener(player => AssignTeam(player, "Blue"));
+        }
+
+        if (runnersClickable != null)
+        {
+            runnersClickable.OnPlayerClick.AddListener(player => AssignTeam(player, "Red"));
+        }
+
+        if (StartGameClickable != null)
+        {
+            StartGameClickable.OnPlayerClick.AddListener(OnplayerClickStart);
+        }
+
+        tokenSelectTeam = this.AddEventHandler(EVENT_SELECT_TEAM, OnTeamSelectEvent);
+        tokenStartGame = this.AddEventHandler(EVENT_START_GAME, OnStartGameEvent);
+        tokenTeleportPlayers = this.AddEventHandler(EVENT_TELEPORT_PLAYERS_INTO_ARENA, OnTeleportPlayers);
+        tokenTeleportPlayers_backToSpawn = this.AddEventHandler(EVENT_TELEPORT_PLAYERS_BACK_TO_SPAWN, OnTeleportPlayersBackToSpawn);
+
+        MassiveLoopRoom.OnPlayerLeft += OnPlayerLeft;
+    }
+
+    private void Update()
+    {
+        if (stopwatch != null && stopwatch.IsRunning)
+        {
+            float remainingTime = TimeLimit - (float)stopwatch.Elapsed.TotalSeconds;
+            if (remainingTime <= 0)
+            {
+                stopwatch.Stop();
+                EndGame();
+            }
+            else
+            {
+                timeText.text = $"Time: \n {remainingTime:F2} \n ";
+            }
+        }
+
+        UpdateTeamText();
+    }
+
+    private void AssignTeam(MLPlayer player, string team)
+    {
+        string playerName = player.NickName;
+
+      //  player.SetProperty("team", team); // Assign the team property
+
+        // Add the player to the playersArray if not already added
+        /*if (!playersArray.Contains(player))
+        {
+            playersArray.Add(player);
+        }*/
+
+        // Synchronize team assignment across all players
+        this.InvokeNetwork(EVENT_SELECT_TEAM, EventTarget.All, null, playerName, team);
+        UpdateTeamLocally(playerName, team);
+    }
+
+    private void OnTeamSelectEvent(object[] args)
+    {
+        string playerName = args[0] as string;
+        string team = args[1] as string;
+
+        UnityEngine.Debug.Log($"Passed in team is : {team}");
+
+        MLPlayer truePlayer = MassiveLoopRoom.FindPlayerByName(playerName);
+        truePlayer.SetProperty("team", team);
+        UnityEngine.Debug.Log($"Player {truePlayer.NickName} team set to : {truePlayer.GetProperty("team")}");
+        UpdateTeamLocally(playerName, team);
+    }
+
+    private void UpdateTeamLocally(string playerName, string team)
+    {
+        if (team == "Blue")
+        {
+            if (!blueTeam.Contains(playerName))
+            {
+                if (redTeam.Contains(playerName)) redTeam.Remove(playerName);
+                blueTeam.Add(playerName);
+            }
+        }
+        else if (team == "Red")
+        {
+            if (!redTeam.Contains(playerName))
+            {
+                if (blueTeam.Contains(playerName)) blueTeam.Remove(playerName);
+                redTeam.Add(playerName);
+            }
+        }
+
+        UpdateTeamText();
+    }
+
+    private void TeleportPlayersToArena()
+    {
+        MLPlayer localPlayer = MassiveLoopRoom.GetLocalPlayer();
+        if (localPlayer.GetProperty("team") != null)
+        {
+            UnityEngine.Debug.Log("Get Property on local player did not return null");
+            if((string)localPlayer.GetProperty("team") == "Red")
+            {
+                UnityEngine.Debug.Log("Red team member found");
+                localPlayer.Teleport(RedTeleportObjectLocation.transform.position);
+            }
+            if((string)localPlayer.GetProperty("team") == "Blue")
+            {
+                UnityEngine.Debug.Log("Blue team member found");
+                localPlayer.Teleport(BlueTeleportObjectLocation.transform.position);
+            }
+        }
+
+        /*foreach (MLPlayer player in playersArray)
+        {
+            string playerName = player.NickName;
+
+            // Use FindPlayerByName to ensure we're working with the correct player object
+            MLPlayer truePlayer = MassiveLoopRoom.FindPlayerByName(playerName);
+
+            // Retrieve the player's team and teleport accordingly
+            string team = (string)truePlayer.GetProperty("team");
+            if ((string)truePlayer.GetProperty("team") == "Blue")
+            {
+                truePlayer.Teleport(BlueTeleportObjectLocation.transform.position);
+            }
+            else if ((string)truePlayer.GetProperty("team") == "Red")
+            {
+                truePlayer.Teleport(RedTeleportObjectLocation.transform.position);
+            }
+        }*/
+    }
+
+    private void TeleportPlayersToSpawn()
+    {
+        /*foreach (MLPlayer player in MassiveLoopRoom.GetAllPlayers())
+        {
+            player.Teleport(RespawnPosition.transform.position); // Teleport back to the spawn location
+        }*/
+
+        MLPlayer localPlayer = MassiveLoopRoom.GetLocalPlayer();
+        localPlayer.Teleport(RespawnPosition.transform.position); // Teleport back to the spawn location
+    }
+
+    private void UpdateTeamText()
+    {
+        BlueTeamString.text = $"Blue Team: \n {string.Join(", ", blueTeam)}  \n";
+        RedTeamString.text = $"Red Team: \n {string.Join(", ", redTeam)}  \n";
+    }
+
+    private void EndGame()
+    {
+        int blueTeamCount = blueTeam.Count;
+        int redTeamCount = redTeam.Count;
+        isGameActive = false;
+
+        if (blueTeamCount > redTeamCount)
+        {
+            GameStatusText.text = "Blue Team Wins!";
+        }
+        else if (redTeamCount > blueTeamCount)
+        {
+            GameStatusText.text = "Red Team Wins!";
+        }
+        else
+        {
+            GameStatusText.text = "It's a Tie!";
+        }
+
+        this.InvokeNetwork(EVENT_TELEPORT_PLAYERS_BACK_TO_SPAWN, EventTarget.All, null);
+        ResetGame();
+    }
+
+    private void ResetGame()
+    {
+        blueTeam.Clear();
+        redTeam.Clear();
+        UpdateTeamText();
+        stopwatch.Reset();
+
+
+        MLPlayer localPlayer = MassiveLoopRoom.GetLocalPlayer();
+        localPlayer.SetProperty("team", "none");
+
+    }
+
+
+    public void OnPlayerHit(MLPlayer player)
+    {
+        string playerName = player.NickName;
+        UnityEngine.Debug.Log($"Player hit, recieved by manager : {player.NickName}");
+
+        if (blueTeam.Contains(playerName))
+        {
+            blueTeam.Remove(playerName);
+        }
+        else if (redTeam.Contains(playerName))
+        {
+            redTeam.Remove(playerName);
+        }
+        //Teleport the player back to spawn
+        player.Teleport(RespawnPosition.transform.position);
+
+    }
+
+
+    private void OnPlayerLeft(MLPlayer player)
+    {
+        string playerName = player.NickName;
+
+        if (blueTeam.Contains(playerName))
+        {
+            blueTeam.Remove(playerName);
+        }
+        else if (redTeam.Contains(playerName))
+        {
+            redTeam.Remove(playerName);
+        }
+
+        // Remove the player from the playersArray
+        /*if (playersArray.Contains(player))
+        {
+            playersArray.Remove(player);
+        }*/
+
+        UpdateTeamText();
+    }
+}
---
+++ DodgeballScript.cs
@@ -0,0 +1,336 @@
+using ML.SDK;
+using UnityEngine;
+
+public class DodgeballScript : MonoBehaviour
+{
+    [SerializeField] private MLGrab grabComponent;
+    [SerializeField] private Rigidbody ballRigidbody;
+    private MLPlayer currentUser;
+
+    // Size limits for size change (relative change)
+    [SerializeField] private float minSize = 0.5f; // Minimum scale factor
+    [SerializeField] private float maxSize = 2.0f; // Maximum scale factor
+
+    // Absolute size limits for local scale
+    [SerializeField] private float absoluteMinSize = 0.3f;
+    [SerializeField] private float absoluteMaxSize = 3.0f;
+
+    [SerializeField] private float throwForceMultiplier = 10.0f; // Multiplier for throw force
+    [SerializeField] private float maxThrowForce = 100.0f; // Cap for throw force magnitude
+
+    [SerializeField] private Material BlueTeamMaterial; // Change to this material whenever a blue player is holding the ball
+    [SerializeField] private Material RedTeamMaterial; // Change to this material whenever a blue player is holding the ball
+    [SerializeField] private Material ResetMaterial; // Change to this material whenever The game gets reset
+    [SerializeField] private GameObject HitEffect;
+
+    [SerializeField] private GameObject DodgeBallGameManagerObjectReference;
+
+    //  [SerializeField] private TrailRenderer LineRenderer; //Change this color to whichever team is holding it.
+
+
+    private Vector3 initialScale;
+    private float initialDistance;
+    private bool isSizeChangeEnabled = false;
+    private bool isHeld = false;
+
+    [SerializeField] private GameObject objectToChange;
+    private Rigidbody objectRigidbody;
+    private Vector3 lastPosition;
+
+    const string EVENT_ID_Throw = "ThrowEvent";
+    private EventToken tokenThrow;
+    public MLPlayer player;
+
+    const string EVENT_HIT = "HitEvent";
+    private EventToken tokenHit;
+
+    private DodgeBallGameManager DodgeBallGameManagerSCRIPTReference;
+
+    private void Start()
+    {
+        // Setup event listeners
+        grabComponent.OnPrimaryGrabBegin.AddListener(OnPrimaryGrabBegin);
+        grabComponent.OnPrimaryGrabEnd.AddListener(OnPrimaryGrabEnd);
+        grabComponent.OnSecondaryGrabBegin.AddListener(OnSecondaryGrabBegin);
+        grabComponent.OnSecondaryGrabEnd.AddListener(OnSecondaryGrabEnd);
+        grabComponent.OnPrimaryTriggerDown.AddListener(OnPrimaryTriggerDown);
+
+        tokenThrow = this.AddEventHandler(EVENT_ID_Throw, OnThrowEvent);
+        tokenHit = this.AddEventHandler(EVENT_HIT, OnHitEvent);
+
+        // Ensure Rigidbody is attached
+        objectRigidbody = objectToChange.GetComponent<Rigidbody>();
+        if (objectRigidbody == null)
+        {
+            Debug.LogError("No Rigidbody component found on the object!");
+        }
+
+        DodgeBallGameManagerSCRIPTReference = (DodgeBallGameManager)DodgeBallGameManagerObjectReference.GetComponent(typeof(DodgeBallGameManager));
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Handle size change during secondary grab
+        if (isSizeChangeEnabled && grabComponent.PrimaryHand && grabComponent.SecondaryHand)
+        {
+            AdjustSizeBasedOnGrab();
+        }
+
+        // Update last position while object is held
+        if (isHeld && grabComponent.PrimaryHand)
+        {
+            lastPosition = grabComponent.PrimaryHand.transform.position;
+        }
+
+        if (DodgeBallGameManagerSCRIPTReference.isGameActive == false)
+        {
+            grabComponent.ForceRelease();
+            ballRigidbody.isKinematic = true;
+        }else if(DodgeBallGameManagerSCRIPTReference.isGameActive == true)
+        {
+            ballRigidbody.isKinematic = false;
+        }
+
+        /*
+        if (MassiveLoopClient.IsInDesktopMode)
+        { 
+            if(player.UserInput != null)
+            {
+                if(player.UserInput.ALT)
+            }
+        }*/
+    }
+
+
+    private void OnPrimaryTriggerDown()
+    {
+        grabComponent.ForceRelease();
+        // this.InvokeNetwork(EVENT_ID_Throw, EventTarget.Master, null);
+    }
+    private void OnPrimaryGrabBegin()
+    {
+        if (grabComponent.CurrentUser != null)
+        {
+            //    player = grabComponent.CurrentUser;
+            isHeld = true;
+            currentUser = grabComponent.CurrentUser;
+            lastPosition = grabComponent.PrimaryHand.transform.position;
+
+            if (grabComponent.GrabMechanic == SDKGrabMechanicsBehaviorV2.Custom)
+            {
+                objectToChange.transform.parent = grabComponent.PrimaryHand.transform;
+            }
+
+
+            if ((string)currentUser.GetProperty("team") == "Blue")
+            {
+                MeshRenderer meshRenderReference = (MeshRenderer)this.gameObject.GetComponent(typeof(MeshRenderer));
+                meshRenderReference.material = BlueTeamMaterial;
+           //     LineRenderer.startColor = Color.blue;
+
+            }else if ((string) currentUser.GetProperty("team") == "Red")
+            {
+                MeshRenderer meshRenderReference = (MeshRenderer)this.gameObject.GetComponent(typeof(MeshRenderer));
+                meshRenderReference.material = RedTeamMaterial;
+            //    LineRenderer.startColor = Color.red;
+
+            }
+        }
+    }
+
+    private void OnPrimaryGrabEnd()
+    {
+        // Unparent the object if using custom grab mechanics
+        if (grabComponent.GrabMechanic == SDKGrabMechanicsBehaviorV2.Custom)
+        {
+            transform.parent = null;
+        }
+
+        if (isHeld && objectRigidbody != null)
+        {
+            // Ensure Rigidbody is dynamic
+            objectRigidbody.isKinematic = false;
+
+            // Calculate the release direction based on the hand's orientation
+            Vector3 releaseDirection = grabComponent.PrimaryHand.transform.forward;
+
+            // Scale the release force
+            Vector3 throwForce = releaseDirection * throwForceMultiplier;
+
+            // Clamp the force to a maximum value
+            if (throwForce.magnitude > maxThrowForce)
+            {
+                throwForce = throwForce.normalized * maxThrowForce;
+            }
+
+            // Apply the force as an impulse
+            objectRigidbody.AddForce(throwForce, ForceMode.Impulse);
+            //  this.InvokeNetwork(EVENT_ID_Throw, EventTarget.Master, null, throwForce);
+
+
+            // Debug visualization for force direction
+            //    Debug.DrawRay(transform.position, releaseDirection * 2.0f, Color.red, 2.0f);
+            Debug.Log($"Object thrown with impulse force: {throwForce}");
+            //   this.InvokeNetwork(EVENT_ID_Throw, EventTarget.Master, null, throwForce);
+        }
+
+        isHeld = false; // Reset the held flag
+    }
+    private void OnSecondaryGrabBegin()
+    {
+        if (grabComponent.PrimaryHand && grabComponent.SecondaryHand)
+        {
+            isSizeChangeEnabled = true;
+            initialDistance = Vector3.Distance(
+                grabComponent.PrimaryHand.transform.position,
+                grabComponent.SecondaryHand.transform.position
+            );
+            initialScale = objectToChange.transform.localScale;
+        }
+    }
+
+    private void OnSecondaryGrabEnd()
+    {
+        isSizeChangeEnabled = false;
+    }
+
+    private void AdjustSizeBasedOnGrab()
+    {
+        // Calculate current distance between hands
+        float currentDistance = Vector3.Distance(
+            grabComponent.PrimaryHand.transform.position,
+            grabComponent.SecondaryHand.transform.position
+        );
+
+        // Calculate scale factor and new scale
+        float scaleFactor = Mathf.Clamp(currentDistance / initialDistance, minSize, maxSize);
+        Vector3 newScale = initialScale * scaleFactor;
+
+        // Clamp to absolute scale limits
+        newScale.x = Mathf.Clamp(newScale.x, absoluteMinSize, absoluteMaxSize);
+        newScale.y = Mathf.Clamp(newScale.y, absoluteMinSize, absoluteMaxSize);
+        newScale.z = Mathf.Clamp(newScale.z, absoluteMinSize, absoluteMaxSize);
+
+        objectToChange.transform.localScale = newScale;
+    }
+
+    private void OnThrowEvent(object[] args)
+    {
+        Debug.Log("Throw event triggered.");
+        objectRigidbody.AddForce((Vector3)args[0], ForceMode.Impulse);
+
+    }
+
+    private void OnHitEvent(object[] args) {
+
+        int throwingPlayer = (int)args[0];
+        int hitPlayer = (int)args[1];
+
+        Object.Instantiate(HitEffect, this.gameObject.transform.position, Quaternion.identity);
+
+        MLPlayer hitplyerMLReference = MassiveLoopRoom.FindPlayerByActorNumber(hitPlayer);
+
+        DodgeBallGameManagerSCRIPTReference.OnPlayerHit(hitplyerMLReference);
+
+       // Debug.Log($"Player : {throwingPlayer} hit player : {hitPlayer} with a dodgeball ");
+    
+    }
+
+
+    private void OnGrabBegin()
+    {
+        currentUser = grabComponent.CurrentUser;
+    }
+
+    private void OnGrabEnd()
+    {
+        currentUser = null; // Reset current user when the ball is released
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the ball collides with a player
+        MLPlayer hitPlayer = (MLPlayer)collision.gameObject.GetPlayer();
+
+        if (hitPlayer != null && currentUser != null)
+        {
+            Debug.Log($"Player found : {hitPlayer.NickName}");
+            // Ensure the hit player is not the current user
+            if (hitPlayer != currentUser)
+            {
+                // Check team property
+                string currentUserTeam = (string)currentUser.GetProperty("team");
+                string hitPlayerTeam = (string)hitPlayer.GetProperty("team");
+
+                if (currentUser != null && hitPlayer != null && hitPlayerTeam != currentUserTeam)
+                {
+                    Debug.Log("Local hit detected from ball thrower attempting to call network function");
+                    this.InvokeNetwork(EVENT_HIT, EventTarget.All, null, currentUser.ActorId, hitPlayer.ActorId);
+
+                    currentUserTeam = "Empty";
+                    hitPlayerTeam = "none";
+                }
+
+
+                if (hitPlayerTeam != currentUserTeam)
+                {
+                   // Debug.Log($"Player from team {hitPlayerTeam} was hit by team {currentUserTeam}!");
+
+                    // Handle game logic for a valid hit
+                    OnPlayerHit(hitPlayer);
+                    currentUser = null;
+                }
+                else
+                {
+                    //Debug.Log("Hit player is on the same team. No action taken.");
+                }
+            }
+        }
+    }
+
+    /*private void OnTriggerEnter(Collider collision)
+    {
+        // Check if the ball collides with a player
+        MLPlayer hitPlayer = (MLPlayer)collision.gameObject.GetPlayer();
+
+        if (hitPlayer != null && currentUser != null)
+        {
+            Debug.Log($"Player found : {hitPlayer.NickName}");
+            // Ensure the hit player is not the current user
+            if (hitPlayer != currentUser)
+            {
+                // Check team property
+                string currentUserTeam = (string)currentUser.GetProperty("team");
+                string hitPlayerTeam = (string)hitPlayer.GetProperty("team");
+
+                if (hitPlayerTeam != currentUserTeam)
+                {
+                    Debug.Log($"Player from team {hitPlayerTeam} was hit by team {currentUserTeam}!");
+
+                    // Handle game logic for a valid hit
+                    OnPlayerHit(hitPlayer);
+                    currentUser = null;
+                }
+                else
+                {
+                    Debug.Log("Hit player is on the same team. No action taken.");
+                }
+            }
+        }
+    }
+    */
+
+    private void OnPlayerHit(MLPlayer hitPlayer)
+    {
+        // Logic for when a player is hit
+       // Debug.Log($"Player {hitPlayer.NickName} has been hit!");
+        this.InvokeNetwork(EVENT_HIT, EventTarget.All, null, currentUser.ActorId, hitPlayer.ActorId);
+        // Example: Disable player temporarily
+        //  hitPlayer.gameObject.SetActive(false);
+
+        // Optionally, add more game-specific logic here (e.g., scoring, respawn mechanics)
+    }
+}
